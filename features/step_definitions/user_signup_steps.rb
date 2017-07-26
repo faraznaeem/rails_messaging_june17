fea34733ddc0e492899ccd9dc7679440ce7bbcd0
faraznaeem <faraznaeem@gmail.com>
2017-07-26 15:51:37 +0200

    passed remaining test, and refactored steps

--- features/step_definitions/user_signup_steps.rb
+++ features/step_definitions/user_signup_steps.rb
@@ -7,13 +7,13 @@ Then(/^I should be on "([^"]*)" page$/) do |page_name|
 end
 
 When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in(field, :with => value)
 end
 
 When(/^I click "([^"]*)" button$/) do |button|
   click_link_or_button(button)
 end
 
-Then(/^I should see the message "([^"]*)"$/) do |arg1|
+Then(/^I should see the message "([^"]*)"$/) do |page|
   expect(page).to have_content
 end
