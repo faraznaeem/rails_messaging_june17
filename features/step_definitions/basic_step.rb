c3684690ac5b4247a83e10a72d99ad99c13cb0e0
j0sv <svensson.jonas@gmail.com>
2017-07-27 21:01:44 +0200

    unifying feature description, test step definitions, and grouping test step definitions

--- features/step_definitions/basic_step.rb
+++ features/step_definitions/basic_step.rb
@@ -1,44 +1,62 @@
-Then(/^show me the page$/) do
-  save_and_open_page
-end
-
+#Action steps
 When(/^I visit "([^"]*)" page$/) do |page_name|
   visit get_path page_name
 end
 
-Then(/^I should be on "([^"]*)" page$/) do |page_name|
-  expect(page).to have_current_path get_path page_name
-end
-
-
-def get_path(page_name)
-
-  case page_name
-  when 'Sign up'
-    path = new_user_registration_path
-  when 'Log in'
-    path = new_user_session_path
-  when 'landing'
-    path = root_path
-  else
-    raise 'Path missing!'
-  end
-
+#Form action steps
+When(/^I click "([^"]*)" (?:link|button)$/) do |link_or_button_name|
+  click_link_or_button link_or_button_name
 end
 
 When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
   fill_in field, with: value
 end
 
-When(/^I click "([^"]*)" button$/) do |button|
-  click_link_or_button button
+Given(/^I select "([^"]*)" from the recipient list$/) do |recipient|
+  select recipient, from: "Recipient"
 end
 
-Then(/^I should see the message "([^"]*)"$/) do |message|
-  expect(page).to have_content message
+#Assertions steps
+Then(/^I should see "([^"]*)"$/) do |content|
+  expect(page).to have_content content
 end
 
+Then(/^I should be on "([^"]*)" page$/) do |page_name|
+  expect(page).to have_current_path get_path page_name
+end
+
+Then(/^show me the page$/) do
+  save_and_open_page
+end
+
+#Background steps
 Given(/^the user "([^"]*)" exist$/) do |name|
   FactoryGirl.create(:user, name: name)
 end
 
+Given(/^the following user exist$/) do |table|
+  table.hashes.each do | user |
+    User.create(user)
+  end
+end
+
+Given(/^I am logged in as "([^"]*)"$/) do |name|
+  user = User.find_by(name: name)
+  login_as(user, scope: :user)
+end
+
+#Step helpers
+def get_path(page_name)
+
+  case page_name
+    when 'Sign up'
+      path = new_user_registration_path
+    when 'Log in'
+      path = new_user_session_path
+    when 'landing'
+      path = root_path
+    else
+      raise 'Path missing!'
+  end
+
+end
