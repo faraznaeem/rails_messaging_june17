65e38636b7ec61c00d355c70357d67445d61eaa3
faraznaeem <faraznaeem@gmail.com>
2017-07-28 10:44:18 +0200

    passed all test for inbox count

--- features/step_definitions/inbox_list_steps.rb
+++ features/step_definitions/inbox_list_steps.rb
@@ -1,9 +1,6 @@
-Given(/^the following users exist$/) do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
-end
 
-Given(/^I send a mail to "([^"]*)"$/) do |arg1|
+Given(/^I send a mail to "([^"]*)"$/) do |name|
+  @user = User.find_by(name: 'bob')
   @receiver = User.find_by(name: name)
   @user.send_message(@receiver, 'Lorem ipsum...', 'Subject')
 end
@@ -13,7 +10,7 @@ Given(/^I am logged in as "([^"]*)"$/) do |name|
   login_as(@user, scope: :user)
 end
 
-Then(/^I should have "([^"]*)" messages$/) do |expected_count|
+Then(/^I should have "([^"]*)" messages$/) do |count|
   count = @receiver.mailbox.inbox.count
-  expect(count).to eq expected_count.to_i
+  expect(count).to eq count.to_i
 end
